059160fcd216fb7000ce8baedda9a580c90d0865
Yichun Zhang (agentzh) <agentzh@gmail.com>
2019-07-06 23:52:46 -0700

    deb: ported the openresty-gdb package with spec2deb.pl.
    also fixed parallelism in previously generated packages by spec2deb.pl.

---
+++ deb/openresty-gdb.mk
@@ -0,0 +1,28 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_GDB_VER := 8.3
+
+.PHONY: openresty-gdb-download
+openresty-gdb-download:
+	wget -nH --cut-dirs=100 --mirror 'https://ftp.gnu.org/gnu/gdb/gdb-$(OPENRESTY_GDB_VER).tar.xz'
+	rm -rf openresty-gdb_$(OPENRESTY_GDB_VER)
+	mkdir -p openresty-gdb_$(OPENRESTY_GDB_VER)
+	tar -xf gdb-8.3.tar.xz --strip-components=1 -C openresty-gdb_$(OPENRESTY_GDB_VER)
+	tar czf openresty-gdb_$(OPENRESTY_GDB_VER).orig.tar.gz openresty-gdb_$(OPENRESTY_GDB_VER)
+
+openresty-gdb-clean:
+	cd openresty-gdb && debclean
+	-find openresty-gdb -maxdepth 1 ! -name 'debian' ! -name 'openresty-gdb' -print | xargs rm -rf
+	rm -rf openresty-gdb*.deb
+	rm -rf openresty-gdb_*.*
+
+.PHONY: openresty-gdb-build
+openresty-gdb-build: openresty-gdb-clean openresty-gdb-download
+	sudo apt-get -y -q install libc6-dev make gcc g++ texinfo libmpfr-dev openresty-python3-dev liblzma-dev ncurses-dev
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-gdb_8.3.orig.tar.gz --strip-components=1 -C openresty-gdb
+	cd openresty-gdb \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi
