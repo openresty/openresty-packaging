77a1a5976b529a9f985276d649b5bd63678f0d26
Yichun Zhang (agentzh) <agentzh@gmail.com>
2019-07-06 22:37:22 -0700

    deb: ported the openresty-python3 package with spec2deb.pl.

---
+++ deb/openresty-python3.mk
@@ -0,0 +1,28 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_PYTHON3_VER := 3.7.3
+
+.PHONY: openresty-python3-download
+openresty-python3-download:
+	wget -nH --cut-dirs=100 --mirror 'https://www.python.org/ftp/python/$(OPENRESTY_PYTHON3_VER)/Python-$(OPENRESTY_PYTHON3_VER).tar.xz'
+	rm -rf openresty-python3_$(OPENRESTY_PYTHON3_VER)
+	mkdir -p openresty-python3_$(OPENRESTY_PYTHON3_VER)
+	tar -xf Python-3.7.3.tar.xz --strip-components=1 -C openresty-python3_$(OPENRESTY_PYTHON3_VER)
+	tar czf openresty-python3_$(OPENRESTY_PYTHON3_VER).orig.tar.gz openresty-python3_$(OPENRESTY_PYTHON3_VER)
+
+openresty-python3-clean:
+	cd openresty-python3 && debclean
+	-find openresty-python3 -maxdepth 1 ! -name 'debian' ! -name 'openresty-python3' -print | xargs rm -rf
+	rm -rf openresty-python3*.deb
+	rm -rf openresty-python3_*.*
+
+.PHONY: openresty-python3-build
+openresty-python3-build: openresty-python3-clean openresty-python3-download
+	sudo apt-get -y -q install libc6-dev gcc libssl-dev make
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-python3_3.7.3.orig.tar.gz --strip-components=1 -C openresty-python3
+	cd openresty-python3 \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi
