07c34c2970bae6f8f3e0ab0b03170886108a941e
Yichun Zhang (agentzh) <yichun@openresty.com>
2020-03-28 21:10:41 -0700

    deb: openresty-yajl.mk: fixed hard-coded version numbers.

--- deb/openresty-yajl.mk
+++ deb/openresty-yajl.mk
@@ -6,7 +6,7 @@ OPENRESTY_YAJL_VER := 2.1.0.3
 .PHONY: openresty-yajl-download
 openresty-yajl-download:
 	rsync -av -e "ssh -o StrictHostKeyChecking=no -o 'UserKnownHostsFile /dev/null'" nuc:~/work/yajl-plus-$(OPENRESTY_YAJL_VER).tar.gz .
-	rsync -av yajl-plus-2.1.0.2.tar.gz openresty-yajl_$(OPENRESTY_YAJL_VER).orig.tar.gz
+	rsync -av yajl-plus-$(OPENRESTY_YAJL_VER).tar.gz openresty-yajl_$(OPENRESTY_YAJL_VER).orig.tar.gz
 
 openresty-yajl-clean:
 	cd openresty-yajl && debclean
@@ -18,7 +18,7 @@ openresty-yajl-clean:
 openresty-yajl-build: openresty-yajl-clean openresty-yajl-download
 	sudo apt-get -y -q install cmake
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	tar xf openresty-yajl_2.1.0.2.orig.tar.gz --strip-components=1 -C openresty-yajl
+	tar xf openresty-yajl_$(OPENRESTY_YAJL_VER).orig.tar.gz --strip-components=1 -C openresty-yajl
 	cd openresty-yajl \
 		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
 		&& debuild $(OPTS) -j$(JOBS)
