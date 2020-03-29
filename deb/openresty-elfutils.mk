53aa3a41676aaf4f4386abe27a6da2c799d0fcb7
Yichun Zhang (agentzh) <yichun@openresty.com>
2020-03-28 21:40:08 -0700

    openresty-elfutils: always upgrade the deps to their latest versions.

--- deb/openresty-elfutils.mk
+++ deb/openresty-elfutils.mk
@@ -17,6 +17,7 @@ openresty-elfutils-clean:
 .PHONY: openresty-elfutils-build
 openresty-elfutils-build: openresty-elfutils-clean openresty-elfutils-download
 	sudo apt-get -y -q install gcc libc6 bison flex m4 gettext zlib1g-dev libbz2-dev liblzma-dev g++ autoconf openresty-yajl-dev gawk sed
+	sudo apt-get -y -q upgrade gcc libc6 bison flex m4 gettext zlib1g-dev libbz2-dev liblzma-dev g++ autoconf openresty-yajl-dev gawk sed
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
 	tar xf openresty-elfutils_$(OPENRESTY_ELFUTILS_VER).orig.tar.gz --strip-components=1 -C openresty-elfutils
 	cd openresty-elfutils \
