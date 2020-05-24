5e74fffe9eee2b33221d17eb0fc9f69b3f16ce4a
Yichun Zhang (agentzh) <yichun@openresty.com>
2020-05-24 14:32:19 -0700

    deb: openresty-gdb.mk: fixed hard-coded gdb version numbers.

--- deb/openresty-gdb.mk
+++ deb/openresty-gdb.mk
@@ -8,7 +8,7 @@ openresty-gdb-download:
 	wget -nH --cut-dirs=100 --mirror 'https://ftp.gnu.org/gnu/gdb/gdb-$(OPENRESTY_GDB_VER).tar.xz'
 	rm -rf openresty-gdb_$(OPENRESTY_GDB_VER)
 	mkdir -p openresty-gdb_$(OPENRESTY_GDB_VER)
-	tar -xf gdb-8.3.tar.xz --strip-components=1 -C openresty-gdb_$(OPENRESTY_GDB_VER)
+	tar -xf gdb-$(OPENRESTY_GDB_VER).tar.xz --strip-components=1 -C openresty-gdb_$(OPENRESTY_GDB_VER)
 	tar czf openresty-gdb_$(OPENRESTY_GDB_VER).orig.tar.gz openresty-gdb_$(OPENRESTY_GDB_VER)
 
 openresty-gdb-clean:
@@ -21,7 +21,7 @@ openresty-gdb-clean:
 openresty-gdb-build: openresty-gdb-clean openresty-gdb-download
 	sudo apt-get -y -q install libc6-dev make gcc g++ texinfo libmpfr-dev openresty-python3-dev liblzma-dev ncurses-dev
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	tar xf openresty-gdb_8.3.orig.tar.gz --strip-components=1 -C openresty-gdb
+	tar xf openresty-gdb_$(OPENRESTY_GDB_VER).orig.tar.gz --strip-components=1 -C openresty-gdb
 	cd openresty-gdb \
 		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
 		&& debuild $(OPTS) -j$(JOBS)
