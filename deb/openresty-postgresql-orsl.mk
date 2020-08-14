d6e8381549fd3be0ef8c0f4e6ede1fe00a6dfd5d
Yichun Zhang (agentzh) <yichun@openresty.com>
2020-08-14 00:03:30 -0700

    bugfix: openresty-postgresql-orsl.mk: avoided hard-coded version numbers.

--- deb/openresty-postgresql-orsl.mk
+++ deb/openresty-postgresql-orsl.mk
@@ -5,10 +5,10 @@ OPENRESTY_POSTGRESQL_ORSL_VER := 0.02
 
 .PHONY: openresty-postgresql-orsl-download
 openresty-postgresql-orsl-download:
-	rsync -av -e "ssh -o StrictHostKeyChecking=no -o 'UserKnownHostsFile /dev/null'" nuc:~/work/postgres-orsl-0.01.tar.gz ./
+	rsync -av -e "ssh -o StrictHostKeyChecking=no -o 'UserKnownHostsFile /dev/null'" nuc:~/work/postgres-orsl-$(OPENRESTY_POSTGRESQL_ORSL_VER).tar.gz ./
 	rm -rf openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER)
 	mkdir -p openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER)
-	tar -xf postgres-orsl-0.01.tar.gz --strip-components=1 -C openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER)
+	tar -xf postgres-orsl-$(OPENRESTY_POSTGRESQL_ORSL_VER).tar.gz --strip-components=1 -C openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER)
 	tar -czf openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER).orig.tar.gz openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER)
 
 openresty-postgresql-orsl-clean:
@@ -21,7 +21,7 @@ openresty-postgresql-orsl-clean:
 openresty-postgresql-orsl-build: openresty-postgresql-orsl-clean openresty-postgresql-orsl-download
 	sudo apt-get -y -q install openresty-postgresql-dev
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	tar xf openresty-postgresql-orsl_0.01.orig.tar.gz --strip-components=1 -C openresty-postgresql-orsl
+	tar xf openresty-postgresql-orsl_$(OPENRESTY_POSTGRESQL_ORSL_VER).orig.tar.gz --strip-components=1 -C openresty-postgresql-orsl
 	cd openresty-postgresql-orsl \
 		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
 		&& debuild $(OPTS) -j$(JOBS)
