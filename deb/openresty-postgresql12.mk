c43d0c51f3ef890fe5ad680ddc217627a26ba6b4
LI Geng <ligeng@openresty.com>
2020-08-20 09:28:01 +0000

    postgresql-timescaledb: add deb packaging scripts for openresty-postgresql12

---
+++ deb/openresty-postgresql12.mk
@@ -0,0 +1,29 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_POSTGRESQL12_VER := 12.3
+
+.PHONY: openresty-postgresql12-download
+openresty-postgresql12-download:
+	wget -nH --cut-dirs=100 --mirror 'https://ftp.postgresql.org/pub/source/v$(OPENRESTY_POSTGRESQL12_VER)/postgresql-$(OPENRESTY_POSTGRESQL12_VER).tar.gz'
+	rm -rf openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER)
+	mkdir -p openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER)
+	tar -xf postgresql-12.3.tar.gz --strip-components=1 -C openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER)
+	rsync -a ../rpm/SOURCES/openresty-postgresql12.init openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER)/
+	tar -czf openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER).orig.tar.gz openresty-postgresql12_$(OPENRESTY_POSTGRESQL12_VER)
+
+openresty-postgresql12-clean:
+	cd openresty-postgresql12 && debclean
+	-find openresty-postgresql12 -maxdepth 1 ! -name 'debian' ! -name 'openresty-postgresql12' -print | xargs rm -rf
+	rm -rf openresty-postgresql12*.deb
+	rm -rf openresty-postgresql12_*.*
+
+.PHONY: openresty-postgresql12-build
+openresty-postgresql12-build: openresty-postgresql12-clean openresty-postgresql12-download
+	sudo apt-get -y -q install ccache libxml2-dev libxslt-dev libossp-uuid-dev libreadline-dev libssl-dev
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-postgresql12_12.3.orig.tar.gz --strip-components=1 -C openresty-postgresql12
+	cd openresty-postgresql12 \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi
