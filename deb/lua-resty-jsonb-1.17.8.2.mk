2aa81dcf5099cc8ee61f4ec1327cf17d56ca2efd
lijunlong <lijunlong@openresty.com>
2020-09-25 00:40:38 +0800

    deb: added new package lua-resty-jsonb-1.17.8.2. (#103)

---
+++ deb/lua-resty-jsonb-1.17.8.2.mk
@@ -0,0 +1,28 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+LUA_RESTY_JSONB_1_17_8_2_VER := 0.0.1
+
+.PHONY: lua-resty-jsonb-1.17.8.2-download
+lua-resty-jsonb-1.17.8.2-download:
+	rsync nuc:~/work/lua-resty-jsonb-0.0.1.tar.gz ./
+	rm -rf lua-resty-jsonb-1.17.8.2_$(LUA_RESTY_JSONB_1_17_8_2_VER)
+	mkdir -p lua-resty-jsonb-1.17.8.2_$(LUA_RESTY_JSONB_1_17_8_2_VER)
+	tar -xf lua-resty-jsonb-0.0.1.tar.gz --strip-components=1 -C lua-resty-jsonb-1.17.8.2_$(LUA_RESTY_JSONB_1_17_8_2_VER)
+	tar -czf lua-resty-jsonb-1.17.8.2_$(LUA_RESTY_JSONB_1_17_8_2_VER).orig.tar.gz lua-resty-jsonb-1.17.8.2_$(LUA_RESTY_JSONB_1_17_8_2_VER)
+
+lua-resty-jsonb-1.17.8.2-clean:
+	cd lua-resty-jsonb-1.17.8.2 && debclean
+	-find lua-resty-jsonb-1.17.8.2 -maxdepth 1 ! -name 'debian' ! -name 'lua-resty-jsonb-1.17.8.2' -print | xargs rm -rf
+	rm -rf lua-resty-jsonb-1.17.8.2*.deb
+	rm -rf lua-resty-jsonb-1.17.8.2_*.*
+
+.PHONY: lua-resty-jsonb-1.17.8.2-build
+lua-resty-jsonb-1.17.8.2-build: lua-resty-jsonb-1.17.8.2-clean lua-resty-jsonb-1.17.8.2-download
+	sudo apt-get -y -q install gcc make openresty-yajl-dev openresty
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf lua-resty-jsonb-1.17.8.2_0.0.1.orig.tar.gz --strip-components=1 -C lua-resty-jsonb-1.17.8.2
+	cd lua-resty-jsonb-1.17.8.2 \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi
