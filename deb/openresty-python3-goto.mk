afdb13ac65af410922caffef4733d1f938231eb3
xlibor@163.com <xlibor@163.com>
2020-10-27 09:52:51 +0800

    deb/rpm: add new package openresty-python3-goto.

---
+++ deb/openresty-python3-goto.mk
@@ -0,0 +1,29 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_PYTHON3_GOTO_VER := 1.2.1
+
+.PHONY: openresty-python3-goto-download
+openresty-python3-goto-download:
+	rsync -a -e "ssh -o StrictHostKeyChecking=no -o 'UserKnownHostsFile /dev/null'" nuc:~/work/python-goto-plus-$(OPENRESTY_PYTHON3_GOTO_VER).tar.gz ./
+	rm -rf openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER)
+	mkdir -p openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER)
+	tar -xf python-goto-plus-$(OPENRESTY_PYTHON3_GOTO_VER).tar.gz --strip-components=1 -C openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER)
+	tar -czf openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER).orig.tar.gz openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER)
+
+openresty-python3-goto-clean:
+	cd openresty-python3-goto && debclean
+	-find openresty-python3-goto -maxdepth 1 ! -name 'debian' ! -name 'openresty-python3-goto' -print | xargs rm -rf
+	rm -rf openresty-python3-goto*.deb
+	rm -rf openresty-python3-goto_*.*
+
+.PHONY: openresty-python3-goto-build
+openresty-python3-goto-build: openresty-python3-goto-clean openresty-python3-goto-download
+	sudo apt-get -y -q install openresty-python3-dev openresty-python3-setuptools
+	sudo apt-get -y -q upgrade openresty-python3-dev openresty-python3-setuptools
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-python3-goto_$(OPENRESTY_PYTHON3_GOTO_VER).orig.tar.gz --strip-components=1 -C openresty-python3-goto
+	cd openresty-python3-goto \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi
---
+++ rpm/SPECS/openresty-python3-goto.spec
@@ -0,0 +1,72 @@
+Name:           openresty-python3-goto
+Version:        1.2.1
+Release:        1%{?dist}
+Summary:        OpenResty's fork of python goto library
+Group:          Development/Libraries
+License:        Public Domain
+URL:            https://github.com/snoack/python-goto
+Source0:        python-goto-plus-%{version}.tar.gz
+
+AutoReqProv: no
+
+%define py_prefix /usr/local/openresty-python3
+%define py_bin %{py_prefix}/bin/python3
+%define py_lib %{py_prefix}/lib/python3.7
+%define py_sitearch %{py_lib}/site-packages
+%define py_version 3.7
+
+%define __jar_repack 0
+%define __brp_mangle_shebangs /usr/bin/true
+%define __brp_python_shebangs /usr/bin/true
+
+%global __python %{py_bin}
+
+%global debug_package %{nil}
+
+
+%if 0%{?fedora} >= 27 || 0%{?rhel} >= 8
+%undefine _debugsource_packages
+%undefine _debuginfo_subpackages
+%endif
+
+
+BuildRequires:  openresty-python3-devel >= 3.7.9-3
+BuildRequires:  openresty-python3-setuptools >= 39.2.0-3
+
+Requires:   openresty-python3 >= 3.7.9-3
+
+
+%description
+OpenResty's fork of python goto.
+A function decorator, that rewrites the bytecode, to enable goto in Python.
+
+
+%prep
+%setup -q -n python-goto-plus-%{version}
+
+
+%build
+PATH="%{py_prefix}/bin:$PATH" %{py_bin} setup.py build %{?_smp_mflags}
+
+
+%install
+PATH="%{py_prefix}/bin:$PATH" %{py_bin} setup.py install --root %{buildroot}
+
+
+# Remove egg-info
+rm -rf %{buildroot}%{py_sitearch}/goto_statement-%{version}-py%{py_version}.egg-info
+
+
+export QA_RPATHS=$[ 0x0002 ]
+
+
+%files
+%defattr(-, root, root)
+%{py_sitearch}/*
+
+
+%clean
+rm -rf %{buildroot}
+
+
+%changelog
