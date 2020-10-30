openresty/openresty-packaging

commit aa9565537082136b55528a5fb62f5e1cc37d7fa7

plus: tcpdump-plus: added new packages openresty-tcpdump and openresty-pcap

deb/openresty-pcap.mk
@@ -1,0 +1,28 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_PCAP_VER := 1.9.1
+
+.PHONY: openresty-pcap-download
+openresty-pcap-download:
+	wget -nH --cut-dirs=100 --mirror 'https://www.tcpdump.org/release/libpcap-$(OPENRESTY_PCAP_VER).tar.gz'
+	rm -rf openresty-pcap_$(OPENRESTY_PCAP_VER)
+	mkdir -p openresty-pcap_$(OPENRESTY_PCAP_VER)
+	tar -xf libpcap-$(OPENRESTY_PCAP_VER).tar.gz --strip-components=1 -C openresty-pcap_$(OPENRESTY_PCAP_VER)
+	tar -czf openresty-pcap_$(OPENRESTY_PCAP_VER).orig.tar.gz openresty-pcap_$(OPENRESTY_PCAP_VER)
+
+openresty-pcap-clean:
+	cd openresty-pcap && debclean
+	-find openresty-pcap -maxdepth 1 ! -name 'debian' ! -name 'openresty-pcap' -print | xargs rm -rf
+	rm -rf openresty-pcap*.deb
+	rm -rf openresty-pcap_*.*
+
+.PHONY: openresty-pcap-build
+openresty-pcap-build: openresty-pcap-clean openresty-pcap-download
+	sudo apt-get -y -q install bison flex
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-pcap_$(OPENRESTY_PCAP_VER).orig.tar.gz --strip-components=1 -C openresty-pcap
+	cd openresty-pcap \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi

deb/openresty-pcap/debian/control
@@ -1,0 +1,44 @@
+Source: openresty-pcap
+Section: libdevel
+Priority: optional
+Maintainer: OpenResty Admin <admin@openresty.com>
+Build-Depends: debhelper (>= 9), bison, flex
+Standards-Version: 3.9.8
+Homepage: http://www.tcpdump.org
+
+Package: openresty-pcap
+Architecture: any
+Section: libdevel
+Depends: ${shlibs:Depends}, ${misc:Depends}
+Description: A system-independent interface for user-level packet capture
+ Libpcap provides a portable framework for low-level network
+ monitoring.  Libpcap can provide network statistics collection,
+ security monitoring and network debugging.  Since almost every system
+ vendor provides a different interface for packet capture, the libpcap
+ authors created this system-independent API to ease in porting and to
+ alleviate the need for several system-dependent packet capture modules
+ in each application.
+ Install libpcap if you need to do low-level network traffic monitoring
+ on your network.
+
+Package: openresty-pcap-dev
+Architecture: any
+Section: libdevel
+Depends: ${shlibs:Depends}, ${misc:Depends}, openresty-pcap (>= ${source:Version})
+Description: Libraries and header files for the libpcap library
+ Libpcap provides a portable framework for low-level network
+ monitoring.  Libpcap can provide network statistics collection,
+ security monitoring and network debugging.  Since almost every system
+ vendor provides a different interface for packet capture, the libpcap
+ authors created this system-independent API to ease in porting and to
+ alleviate the need for several system-dependent packet capture modules
+ in each application.
+ This package provides the libraries, include files, and other
+ resources needed for developing libpcap applications.
+
+Package: openresty-pcap-dbgsym
+Section: debug
+Architecture: any
+Depends: ${misc:Depends}, openresty-pcap (>= ${source:Version})
+Description: Debug symbols for the openresty-pcap package
+ Debug symbols (or debuginfo) for the openresty-pcap package.

deb/openresty-pcap/debian/openresty-pcap-dev.install
@@ -1,0 +1,1 @@
+usr/local/openresty-pcap/include/

deb/openresty-pcap/debian/openresty-pcap.install
@@ -1,0 +1,2 @@
+usr/local/openresty-pcap/lib/libpcap.so*
+usr/local/openresty-pcap/lib/libpcap.a

deb/openresty-pcap/debian/rules
@@ -1,0 +1,32 @@
+#!/usr/bin/make -f
+
+DPKG_EXPORT_BUILDFLAGS = 1
+include /usr/share/dpkg/default.mk
+
+export NO_PKG_MANGLE=1
+export QA_RPATHS=$[ 0x0002 ]
+
+DESTDIR = $(CURDIR)/debian/tmp
+
+%:
+	dh $@ --parallel --with autotools-dev,systemd
+
+override_dh_auto_test:
+
+override_dh_usrlocal:
+
+override_dh_auto_configure:
+
+override_dh_auto_build:
+	export CFLAGS="-fno-strict-aliasing" \
+		&& ./configure --prefix=/usr/local/openresty-pcap --libdir=/usr/local/openresty-pcap/lib \
+		&& make -j`nproc`
+
+override_dh_auto_install:
+	make DESTDIR=$(DESTDIR) install \
+		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/bin \
+		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/share \
+		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/lib/pkgconfig
+
+override_dh_strip:
+	dh_strip --dbg-package=openresty-pcap-dbgsym

deb/openresty-tcpdump.mk
@@ -1,0 +1,25 @@
+## Author: spec2deb.pl
+### Version: 0.01
+
+OPENRESTY_TCPDUMP_VER := 4.9.3.1
+
+.PHONY: openresty-tcpdump-download
+openresty-tcpdump-download:
+	rsync -av -e "ssh -o StrictHostKeyChecking=no -o 'UserKnownHostsFile /dev/null'" nuc:~/work/tcpdump-plus-$(OPENRESTY_TCPDUMP_VER).tar.gz .
+	rsync -av tcpdump-plus-$(OPENRESTY_TCPDUMP_VER).tar.gz openresty-tcpdump_$(OPENRESTY_TCPDUMP_VER).orig.tar.gz
+
+openresty-tcpdump-clean:
+	cd openresty-tcpdump && debclean
+	-find openresty-tcpdump -maxdepth 1 ! -name 'debian' ! -name 'openresty-tcpdump' -print | xargs rm -rf
+	rm -rf openresty-tcpdump*.deb
+	rm -rf openresty-tcpdump_*.*
+
+.PHONY: openresty-tcpdump-build
+openresty-tcpdump-build: openresty-tcpdump-clean openresty-tcpdump-download
+	sudo apt-get -y -q install automake openresty-pcap openresty-pcap-dev
+	rm -f *.deb *.debian.tar.xz *.dsc *.changes
+	tar xf openresty-tcpdump_$(OPENRESTY_TCPDUMP_VER).orig.tar.gz --strip-components=1 -C openresty-tcpdump
+	cd openresty-tcpdump \
+		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
+		&& debuild $(OPTS) -j$(JOBS)
+	#if [ -f ./upload ]; then ./upload || exit 1; fi

deb/openresty-tcpdump/debian/control
@@ -1,0 +1,25 @@
+Source: openresty-tcpdump
+Section: net
+Priority: optional
+Maintainer: OpenResty Admin <admin@openresty.com>
+Build-Depends: debhelper (>= 9), automake, openresty-pcap (>= 1.9.1)
+Standards-Version: 3.9.8
+Homepage: http://www.tcpdump.org
+
+Package: openresty-tcpdump
+Architecture: any
+Section: net
+Depends: ${shlibs:Depends}, ${misc:Depends}, openresty-pcap (>= 1.9.1)
+Description: A network traffic monitoring tool
+ Tcpdump is a command-line tool for monitoring network traffic.
+ Tcpdump can capture and display the packet headers on a particular
+ network interface or on all interfaces.  Tcpdump can display all of
+ the packet headers, or just the ones that match particular criteria.
+ Install tcpdump if you need a program to monitor network traffic.
+
+Package: openresty-tcpdump-dbgsym
+Section: debug
+Architecture: any
+Depends: ${misc:Depends}, openresty-tcpdump (>= 4.9.3.1)
+Description: Debug symbols for the openresty-tcpdump package
+ Debug symbols (or debuginfo) for the openresty-tcpdump package.

deb/openresty-tcpdump/debian/openresty-tcpdump.install
@@ -1,0 +1,1 @@
+usr/local/openresty-tcpdump/sbin/tcpdump

commit 8ea81b1edc60f14f162334395fbeec86ec747056

bugfix: openresty-{pcap,tcpdump}: fixed the compilation errors introduced in commit aa95655.

deb/openresty-pcap.mk
@@ -19,8 +19,8 @@ openresty-pcap-clean:
 
 .PHONY: openresty-pcap-build
 openresty-pcap-build: openresty-pcap-clean openresty-pcap-download
-	sudo apt-get -y -q install bison flex
-	sudo apt-get -y -q --only-upgrade install bison flex
+	sudo apt-get -y -q install bison flex ccache
+	sudo apt-get -y -q --only-upgrade install bison flex ccache
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
 	tar xf openresty-pcap_$(OPENRESTY_PCAP_VER).orig.tar.gz --strip-components=1 -C openresty-pcap
 	cd openresty-pcap \

deb/openresty-pcap/debian/control
@@ -2,7 +2,7 @@ Source: openresty-pcap
 Section: libdevel
 Priority: optional
 Maintainer: OpenResty Admin <admin@openresty.com>
-Build-Depends: debhelper (>= 9), bison, flex
+Build-Depends: debhelper (>= 9), bison, flex, ccache
 Standards-Version: 3.9.8
 Homepage: http://www.tcpdump.org
 

deb/openresty-pcap/debian/openresty-pcap-dev.install
@@ -1,1 +1,3 @@
 usr/local/openresty-pcap/include/
+usr/local/openresty-pcap/lib/libpcap.a
+usr/local/openresty-pcap/lib/libpcap.so

deb/openresty-pcap/debian/openresty-pcap.install
@@ -1,2 +1,1 @@
-usr/local/openresty-pcap/lib/libpcap.so*
-usr/local/openresty-pcap/lib/libpcap.a
+usr/local/openresty-pcap/lib/libpcap.so.*

deb/openresty-pcap/debian/rules
@@ -18,7 +18,7 @@ override_dh_usrlocal:
 override_dh_auto_configure:
 
 override_dh_auto_build:
-	export CFLAGS="-fno-strict-aliasing" \
+	export CFLAGS="-fno-strict-aliasing -g3" \
 		&& ./configure --prefix=/usr/local/openresty-pcap --libdir=/usr/local/openresty-pcap/lib \
 		&& make -j`nproc`
 
@@ -27,6 +27,10 @@ override_dh_auto_install:
 		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/bin \
 		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/share \
 		&& rm -rf $(DESTDIR)/usr/local/openresty-pcap/lib/pkgconfig
+
+override_dh_auto_clean:
+	dh_clean
+	rm -rf $(DESTDIR)
 
 override_dh_strip:
 	dh_strip --dbg-package=openresty-pcap-dbgsym

deb/openresty-tcpdump.mk
@@ -1,7 +1,7 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
-OPENRESTY_TCPDUMP_VER := 4.9.3.1
+OPENRESTY_TCPDUMP_VER := 4.9.3.2
 
 .PHONY: openresty-tcpdump-download
 openresty-tcpdump-download:
@@ -16,7 +16,8 @@ openresty-tcpdump-clean:
 
 .PHONY: openresty-tcpdump-build
 openresty-tcpdump-build: openresty-tcpdump-clean openresty-tcpdump-download
-	sudo apt-get -y -q install automake openresty-pcap openresty-pcap-dev
+	sudo apt-get -y -q install automake openresty-pcap-dev ccache
+	sudo apt-get -y -q --only-upgrade install  automake openresty-pcap-dev ccache
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
 	tar xf openresty-tcpdump_$(OPENRESTY_TCPDUMP_VER).orig.tar.gz --strip-components=1 -C openresty-tcpdump
 	cd openresty-tcpdump \

deb/openresty-tcpdump/debian/control
@@ -2,7 +2,7 @@ Source: openresty-tcpdump
 Section: net
 Priority: optional
 Maintainer: OpenResty Admin <admin@openresty.com>
-Build-Depends: debhelper (>= 9), automake, openresty-pcap (>= 1.9.1)
+Build-Depends: debhelper (>= 9), automake, openresty-pcap-dev (>= 1.9.1), ccache
 Standards-Version: 3.9.8
 Homepage: http://www.tcpdump.org
 
@@ -15,11 +15,11 @@ Description: A network traffic monitoring tool
  Tcpdump can capture and display the packet headers on a particular
  network interface or on all interfaces.  Tcpdump can display all of
  the packet headers, or just the ones that match particular criteria.
- Install tcpdump if you need a program to monitor network traffic.
+ Install tcpdump-plus if you need a program to monitor network traffic.
 
 Package: openresty-tcpdump-dbgsym
 Section: debug
 Architecture: any
-Depends: ${misc:Depends}, openresty-tcpdump (>= 4.9.3.1)
+Depends: ${misc:Depends}, openresty-tcpdump (>= 4.9.3.2)
 Description: Debug symbols for the openresty-tcpdump package
  Debug symbols (or debuginfo) for the openresty-tcpdump package.

deb/openresty-tcpdump/debian/openresty-tcpdump.install
@@ -1,1 +1,1 @@
-usr/local/openresty-tcpdump/sbin/tcpdump
+usr/local/openresty-tcpdump/sbin/tcpdump-plus

deb/openresty-tcpdump/debian/rules
@@ -17,17 +17,21 @@ override_dh_usrlocal:
 
 override_dh_auto_configure:
 
-	#export CC="ccache gcc -fdiagnostics-color=always -I/usr/local/openresty-pcap/include" 
 override_dh_auto_build:
 	export CC="ccache gcc -I/usr/local/openresty-pcap/include" \
-		&& export CFLAGS="$(getconf LFS_CFLAGS) -fno-strict-aliasing -DHAVE_GETNAMEINFO -I/usr/local/openresty-pcap/include" \
+		&& export CFLAGS="-g3 $(getconf LFS_CFLAGS) -fno-strict-aliasing -DHAVE_GETNAMEINFO -I/usr/local/openresty-pcap/include" \
 		&& export LDFLAGS="-L/usr/local/openresty-pcap/lib -Wl,-rpath,/usr/local/openresty-pcap/lib" \
-		&& ./configure --with-user=tcpdump --without-smi --prefix=/usr/local/openresty-tcpdump \
+		&& ./configure --with-user=tcpdump --without-smi --prefix=/usr/local/openresty-tcpdump --with-system-libpcap \
 		&& make -j`nproc`
 
 override_dh_auto_install:
-	mkdir -p $(DESTDIR)/usr/local/openresty-tcpdump/sbin \
-		&& install -m755 tcpdump $(DESTDIR)/usr/local/openresty-tcpdump/sbin
+	rm -rf $(DESTDIR) \
+		&& mkdir -p $(DESTDIR)/usr/local/openresty-tcpdump/sbin \
+		&& install -m755 tcpdump-plus $(DESTDIR)/usr/local/openresty-tcpdump/sbin
+
+override_dh_auto_clean:
+	dh_clean
+	rm -rf $(DESTDIR)
 
 override_dh_strip:
 	dh_strip --dbg-package=openresty-tcpdump-dbgsym

rpm/SPECS/openresty-pcap.spec
@@ -5,9 +5,8 @@ Summary: A system-independent interface for user-level packet capture
 Group: Development/Libraries
 License: BSD with advertising
 URL: http://www.tcpdump.org
-BuildRequires: glibc-kernheaders >= 2.2.0
-BuildRequires: bison
-BuildRequires: flex
+BuildRequires: glibc-kernheaders >= 2.2.0, bison, flex, ccache
+AutoReqProv: no
 
 %define  pcap_prefix /usr/local/openresty-pcap
 
@@ -30,6 +29,20 @@ Summary: Libraries and header files for the libpcap library
 Group: Development/Libraries
 Requires: %{name} = %{version}-%{release}
 
+# Remove source code from debuginfo package.
+%define __debug_install_post \
+  %{_rpmconfigdir}/find-debuginfo.sh %{?_missing_build_ids_terminate_build:--strict-build-id} %{?_find_debuginfo_opts} "%{_builddir}/%{?buildsubdir}"; \
+  rm -rf "${RPM_BUILD_ROOT}/usr/src/debug"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/libpcap-%{version}"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/tmp"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/builddir"; \
+%{nil}
+
+%if 0%{?fedora} >= 27 || 0%{?rhel} >= 8
+%undefine _debugsource_packages
+%undefine _debuginfo_subpackages
+%endif
+
 %description devel
 Libpcap provides a portable framework for low-level network
 monitoring.  Libpcap can provide network statistics collection,
@@ -45,13 +58,8 @@ resources needed for developing libpcap applications.
 %prep
 %setup -q -n libpcap-%{version}
 
-#sparc needs -fPIC
-%ifarch %{sparc}
-sed -i -e 's|-fpic|-fPIC|g' configure
-%endif
-
 %build
-export CFLAGS="$RPM_OPT_FLAGS -fno-strict-aliasing"
+export CFLAGS="$RPM_OPT_FLAGS -fno-strict-aliasing -g3"
 ./configure --prefix=%{pcap_prefix} --libdir=%{pcap_prefix}/lib
 make %{?_smp_mflags}
 
@@ -61,18 +69,18 @@ rm -rf %{buildroot}/%{pcap_prefix}/bin
 rm -rf %{buildroot}/%{pcap_prefix}/share
 rm -rf %{buildroot}/%{pcap_prefix}/lib/pkgconfig
 
-%post -p /sbin/ldconfig
-
-%postun -p /sbin/ldconfig
+%clean
+rm -rf $RPM_BUILD_ROOT
 
 %files
 %defattr(-,root,root)
-%{pcap_prefix}/lib/libpcap.so*
-%{pcap_prefix}/lib/libpcap.a
+%{pcap_prefix}/lib/libpcap.so.*
 
 %files devel
 %defattr(-,root,root)
 %{pcap_prefix}/include/
+%{pcap_prefix}/lib/libpcap.a
+%{pcap_prefix}/lib/libpcap.so
 
 %changelog
 * Wed Jun 17 2020 zhuizhuhaomeng 1.9.1

rpm/SPECS/openresty-tcpdump.spec
@@ -7,13 +7,27 @@ URL: http://www.tcpdump.org
 Group: Applications/Internet
 #Requires(pre): shadow-utils /usr/bin/getent
 Requires: openresty-pcap >= 1.9.1
-BuildRequires: automake
-BuildRequires: openresty-pcap >= 1.9.1
+BuildRequires: automake, openresty-pcap-devel >= 1.9.1, ccache
+AutoReqProv: no
 
 %define tcpdump_prefix     /usr/local/openresty-tcpdump
 %define pcap_prefix        /usr/local/openresty-pcap
 
 Source0: tcpdump-plus-%{version}.tar.gz
+
+# Remove source code from debuginfo package.
+%define __debug_install_post \
+  %{_rpmconfigdir}/find-debuginfo.sh %{?_missing_build_ids_terminate_build:--strict-build-id} %{?_find_debuginfo_opts} "%{_builddir}/%{?buildsubdir}"; \
+  rm -rf "${RPM_BUILD_ROOT}/usr/src/debug"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/tcpdump-plus-%{version}"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/tmp"; \
+  mkdir -p "${RPM_BUILD_ROOT}/usr/src/debug/builddir"; \
+%{nil}
+
+%if 0%{?fedora} >= 27 || 0%{?rhel} >= 8
+%undefine _debugsource_packages
+%undefine _debuginfo_subpackages
+%endif
 
 %description
 Tcpdump is a command-line tool for monitoring network traffic.
@@ -21,7 +35,7 @@ Tcpdump can capture and display the packet headers on a particular
 network interface or on all interfaces.  Tcpdump can display all of
 the packet headers, or just the ones that match particular criteria.
 
-Install tcpdump if you need a program to monitor network traffic.
+Install tcpdump-plus if you need a program to monitor network traffic.
 
 %prep
 %setup -q -n tcpdump-plus-%{version}
@@ -29,16 +43,16 @@ Install tcpdump if you need a program to monitor network traffic.
 %build
 
 export CC="ccache gcc -fdiagnostics-color=always -I%{pcap_prefix}/include"
-export CFLAGS="$RPM_OPT_FLAGS $(getconf LFS_CFLAGS) -fno-strict-aliasing -DHAVE_GETNAMEINFO -I%{pcap_prefix}/include"
+export CFLAGS="-g3 $RPM_OPT_FLAGS $(getconf LFS_CFLAGS) -fno-strict-aliasing -DHAVE_GETNAMEINFO -I%{pcap_prefix}/include"
 export LDFLAGS="-L%{pcap_prefix}/lib -Wl,-rpath,%{pcap_prefix}/lib"
 
-./configure --with-user=tcpdump --without-smi --prefix=%{tcpdump_prefix}
+./configure --with-user=tcpdump --without-smi --prefix=%{tcpdump_prefix} --with-system-libpcap
 make %{?_smp_mflags}
 
-
 %install
+rm -rf $RPM_BUILD_ROOT
 mkdir -p ${RPM_BUILD_ROOT}%{tcpdump_prefix}/sbin
-install -m755 tcpdump ${RPM_BUILD_ROOT}%{tcpdump_prefix}/sbin
+install -m755 tcpdump-plus ${RPM_BUILD_ROOT}%{tcpdump_prefix}/sbin
 
 
 %pre
@@ -50,9 +64,12 @@ fi
 
 exit 0
 
+%clean
+rm -rf $RPM_BUILD_ROOT
+
 %files
 %defattr(-,root,root)
-%{tcpdump_prefix}/sbin/tcpdump
+%{tcpdump_prefix}/sbin/tcpdump-plus
 
 %changelog
 * Wed Oct 28 2020 Yichun Zhang (agentzh) 4.9.3.2-1