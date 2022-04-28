2cf9d32e5d3543b52f5b08033214288e24fd4ed4
Yichun Zhang (agentzh) <yichun@openresty.com>
2022-04-27 22:32:40 -0700

    plus: openresty-elfutils: upgraded elfutils-plus to 0.185.3.

--- deb/openresty-elfutils.mk
+++ deb/openresty-elfutils.mk
@@ -1,7 +1,7 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
-OPENRESTY_ELFUTILS_VER := 0.185.2
+OPENRESTY_ELFUTILS_VER := 0.185.3
 
 .PHONY: openresty-elfutils-download
 openresty-elfutils-download:
