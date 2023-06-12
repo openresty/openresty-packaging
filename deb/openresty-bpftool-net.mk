1189dcaac3c2bf568a90e4a7578a784b1b9be5d1
Yichun Zhang (agentzh) <yichun@openresty.com>
2023-06-12 16:33:26 -0700

    deb: openresty-bpftool-net: more fixes.

--- deb/openresty-bpftool-net.mk
+++ deb/openresty-bpftool-net.mk
@@ -12,9 +12,9 @@ openresty-bpftool-net-download:
 	tar -czf openresty-bpftool-net_$(OPENRESTY_BPFTOOL_NET_VER).orig.tar.gz openresty-bpftool-net_$(OPENRESTY_BPFTOOL_NET_VER)
 
 openresty-bpftool-net-clean:
-	-cd openresty-bpftool && debclean
-	-find openresty-bpftool -maxdepth 1 ! -name 'debian' ! -name 'openresty-bpftool' -print | xargs rm -rf
-	rm -rf openresty-bpftool*.deb
+	-cd openresty-bpftool-net && debclean
+	-find openresty-bpftool-net -maxdepth 1 ! -name 'debian' ! -name 'openresty-bpftool-net' -print | xargs rm -rf
+	rm -rf openresty-bpftool-net*.deb
 	rm -rf openresty-bpftool-net_*.*
 
 #sudo apt-get -y -q install libtemplate-perl debhelper devscripts dh-systemd
@@ -26,8 +26,8 @@ openresty-bpftool-net-build: openresty-bpftool-net-clean openresty-bpftool-net-d
 	sudo apt-get -y -q install --only-upgrade ccache gcc make perl pkg-config openresty-libbpf-net-dev \
 		openresty-elfutils-dev openresty-binutils-dev libcap-dev openresty-zlib-dev vim-common
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	tar xf openresty-bpftool-net_$(OPENRESTY_BPFTOOL_NET_VER).orig.tar.gz --strip-components=1 -C openresty-bpftool
-	cd openresty-bpftool \
+	tar xf openresty-bpftool-net_$(OPENRESTY_BPFTOOL_NET_VER).orig.tar.gz --strip-components=1 -C openresty-bpftool-net
+	cd openresty-bpftool-net \
 		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
 		&& tpage --define distro=$(DISTRO) debian/control.tt2 > debian/control \
 		&& debuild --no-lintian $(OPTS) -j$(JOBS)
