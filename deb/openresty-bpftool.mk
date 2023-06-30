64fa41e0361564a518ced1e9397850adece5ed30
Yichun Zhang (agentzh) <yichun@openresty.com>
2023-06-29 20:09:48 -0700

    plus: openresty-bpftool: upgraded bpftool-plus to 5.13.18.8.

--- deb/openresty-bpftool.mk
+++ deb/openresty-bpftool.mk
@@ -1,7 +1,7 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
-OPENRESTY_BPFTOOL_VER := 5.13.18.7
+OPENRESTY_BPFTOOL_VER := 5.13.18.8
 
 .PHONY: openresty-bpftool-download
 openresty-bpftool-download:
