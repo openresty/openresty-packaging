4610d42463536a6d2eba9bc8d2c7cb608c3aa8b6
lijunlong <lijunlong@openresty.com>
2023-11-07 09:43:57 +0800

    bugfix: In the shell, the == operator of an if statement is not portable.
    if [ "arm64" == "arm64" ] && [ ! -e /usr/include/asm ]; then echo match; fi
    /bin/sh: 1: [: arm64: unexpected operator

--- deb/openresty-xdp-tools.mk
+++ deb/openresty-xdp-tools.mk
@@ -2,8 +2,6 @@
 ### Version: 0.01
 
 OPENRESTY_XDP_TOOLS_VER := 1.2.2.1
-os_arch = $(shell arch)
-arch_asm = /usr/include/$(os_arch)-linux-gnu/asm
 
 X64_DEP_LIBS =
 ifeq ($(ARCH), amd64)
@@ -28,11 +26,10 @@ openresty-xdp-tools-clean:
 #sudo apt-get -y -q install --only-upgrade libtemplate-perl debhelper devscripts dh-systemd
 .PHONY: openresty-xdp-tools-build
 openresty-xdp-tools-build: openresty-xdp-tools-clean openresty-xdp-tools-download
-	if [ -e $(arch_asm) ] && [ ! -e /usr/include/asm ]; then sudo ln -s $(arch_asm) /usr/include/asm; fi
 	sudo apt-get -y -q install ccache gcc make perl pkg-config openresty-libbpf-net-dev openresty-pcap-dev openresty-elfutils-dev openresty-llvm zlib1g-dev $(X64_DEP_LIBS)
 	sudo apt-get -y -q install --only-upgrade ccache gcc make perl pkg-config openresty-libbpf-net-dev openresty-pcap-dev openresty-elfutils-dev openresty-llvm zlib1g-dev $(X64_DEP_LIBS)
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	if [ "$(ARCH)" == "arm64" ] && [ ! -e /usr/include/asm ]; then sudo ln -s /usr/include/`uname -m`-linux-gnu/asm /usr/include/asm; fi
+	if [ "$(ARCH)" = "arm64" ] && [ ! -e /usr/include/asm ]; then sudo ln -s /usr/include/`uname -m`-linux-gnu/asm /usr/include/asm; fi
 	tar xf openresty-xdp-tools_$(OPENRESTY_XDP_TOOLS_VER).orig.tar.gz --strip-components=1 -C openresty-xdp-tools
 	cd openresty-xdp-tools \
 		&& tpage --define arch=$(ARCH) --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
