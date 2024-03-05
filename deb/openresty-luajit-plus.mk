cf372a4fbc5ec2ffcdf7428cbd81e23fe7bfda99
Yichun Zhang (agentzh) <yichun@openresty.com>
2024-03-04 22:50:13 -0800

    plus: openresty-luajit-plus: upgraded openresty-luajit-plus to 20240229.

--- deb/openresty-luajit-plus.mk
+++ deb/openresty-luajit-plus.mk
@@ -1,7 +1,7 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
-OPENRESTY_LUAJIT_PLUS_VER := 20231123
+OPENRESTY_LUAJIT_PLUS_VER := 20240229
 
 .PHONY: openresty-luajit-plus-download
 openresty-luajit-plus-download:
