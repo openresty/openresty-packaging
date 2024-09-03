042b0e33b4e9dd06c34f3608cae189108c9e73cf
lijunlong <lijunlong@openresty.com>
2024-09-03 12:15:08 +0800

    bugfix: fixed typo in update-lua-resty-traffic-limit-dynamic.

--- deb/lua-resty-limit-traffic-dynamic.mk
+++ deb/lua-resty-limit-traffic-dynamic.mk
@@ -1,7 +1,7 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
- LUA_RESTY_LIMIT_TRAFFIC_DYNAMIC_VE := 1.0.2
+ LUA_RESTY_LIMIT_TRAFFIC_DYNAMIC_VER := 1.0.2
 
 .PHONY: lua-resty-limit-traffic-dynamic-download
 lua-resty-limit-traffic-dynamic-download:
