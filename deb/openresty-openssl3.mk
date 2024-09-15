c29bdd9fac761abe33cb102e9de982ad11b1b8e6
lijunlong <lijunlong@openresty.com>
2024-09-15 10:34:32 +0800

    add asan&debug support.

--- deb/openresty-openssl3.mk
+++ deb/openresty-openssl3.mk
@@ -1,31 +1,31 @@
 ## Author: spec2deb.pl
 ### Version: 0.01
 
-OPENRESTY_OPENSSL30_VER := 3.0.15
+OPENRESTY_OPENSSL3_VER := 3.0.15
 
-.PHONY: openresty-openssl30-download
-openresty-openssl30-download:
-	LANG=C LC_ALL='C.UTF-8' wget -nH --cut-dirs=100 --mirror 'https://github.com/openssl/openssl/releases/download/openssl-$(OPENRESTY_OPENSSL30_VER)/openssl-$(OPENRESTY_OPENSSL30_VER).tar.gz'
-	rm -rf openresty-openssl30_$(OPENRESTY_OPENSSL30_VER)
-	mkdir -p openresty-openssl30_$(OPENRESTY_OPENSSL30_VER)
-	tar -xf openssl-$(OPENRESTY_OPENSSL30_VER).tar.gz --strip-components=1 -C openresty-openssl30_$(OPENRESTY_OPENSSL30_VER)
-	tar -czf openresty-openssl30_$(OPENRESTY_OPENSSL30_VER).orig.tar.gz openresty-openssl30_$(OPENRESTY_OPENSSL30_VER)
+.PHONY: openresty-openssl3-download
+openresty-openssl3-download:
+	LANG=C LC_ALL='C.UTF-8' wget -nH --cut-dirs=100 --mirror 'https://github.com/openssl/openssl/releases/download/openssl-$(OPENRESTY_OPENSSL3_VER)/openssl-$(OPENRESTY_OPENSSL3_VER).tar.gz'
+	rm -rf openresty-openssl3_$(OPENRESTY_OPENSSL3_VER)
+	mkdir -p openresty-openssl3_$(OPENRESTY_OPENSSL3_VER)
+	tar -xf openssl-$(OPENRESTY_OPENSSL3_VER).tar.gz --strip-components=1 -C openresty-openssl3_$(OPENRESTY_OPENSSL3_VER)
+	tar -czf openresty-openssl3_$(OPENRESTY_OPENSSL3_VER).orig.tar.gz openresty-openssl3_$(OPENRESTY_OPENSSL3_VER)
 
-openresty-openssl30-clean:
-	-cd openresty-openssl30 && debclean
-	-find openresty-openssl30 -maxdepth 1 ! -name 'debian' ! -name 'openresty-openssl30' -print | xargs rm -rf
-	rm -rf openresty-openssl30*.deb
-	rm -rf openresty-openssl30_*.*
+openresty-openssl3-clean:
+	-cd openresty-openssl3 && debclean
+	-find openresty-openssl3 -maxdepth 1 ! -name 'debian' ! -name 'openresty-openssl3' -print | xargs rm -rf
+	rm -rf openresty-openssl3*.deb
+	rm -rf openresty-openssl3_*.*
 
 #sudo apt-get -y -q install libtemplate-perl debhelper devscripts dh-systemd
 #sudo apt-get -y -q install --only-upgrade libtemplate-perl debhelper devscripts dh-systemd
-.PHONY: openresty-openssl30-build
-openresty-openssl30-build: openresty-openssl30-clean openresty-openssl30-download
+.PHONY: openresty-openssl3-build
+openresty-openssl3-build: openresty-openssl3-clean openresty-openssl3-download
 	sudo apt-get -y -q install gcc make perl openresty-zlib-dev
 	sudo apt-get -y -q install --only-upgrade gcc make perl openresty-zlib-dev
 	rm -f *.deb *.debian.tar.xz *.dsc *.changes
-	tar xf openresty-openssl30_$(OPENRESTY_OPENSSL30_VER).orig.tar.gz --strip-components=1 -C openresty-openssl30
-	cd openresty-openssl30 \
+	tar xf openresty-openssl3_$(OPENRESTY_OPENSSL3_VER).orig.tar.gz --strip-components=1 -C openresty-openssl3
+	cd openresty-openssl3 \
 		&& tpage --define distro=$(DISTRO) debian/changelog.tt2 > debian/changelog \
 		&& debuild --no-lintian $(OPTS) -j$(JOBS)
 	#if [ -f ./upload ]; then ./upload || exit 1; fi
--- rpm/SPECS/openresty-asan.spec
+++ rpm/SPECS/openresty-asan.spec
@@ -19,17 +19,17 @@ BuildRequires:  ccache, make, perl, systemtap-sdt-devel, gcc, valgrind-devel
 
 BuildRequires:  perl-File-Temp
 BuildRequires:  openresty-zlib-asan-devel >= 1.2.12-1
-BuildRequires:  openresty-openssl111-asan-devel >= 1.1.1w-1
+BuildRequires:  openresty-openssl3-asan-devel >= 1.1.1w-1
 BuildRequires:  openresty-pcre2-asan-devel >= 10.42-1
 
 Requires:       openresty-zlib-asan >= 1.2.12-1
-Requires:       openresty-openssl111-asan >= 1.1.1w-1
+Requires:       openresty-openssl3-asan >= 1.1.1w-1
 Requires:       openresty-pcre2-asan >= 10.42-1
 
 AutoReqProv:        no
 
 %define orprefix            %{_usr}/local/%{name}
-%define openssl_prefix      %{_usr}/local/openresty-asan/openssl111
+%define openssl_prefix      %{_usr}/local/openresty-asan/openssl3
 %define zlib_prefix         %{_usr}/local/openresty-asan/zlib
 %define pcre_prefix         %{_usr}/local/openresty-asan/pcre2
 
